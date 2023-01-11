86827b172689cf9697da037844ac07b82f08e1b6
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-10 22:41:46 -0300

    feat: add user login route

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,8 +1,19 @@
-import { User, Client } from "../db/config";
+import { User } from "../db/config";
 import { Express, Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export class UserController {
+const { JWT_SECRET } = process.env;
+
+interface SignInRequest extends Request {
+  body: { username: string; password: string };
+}
+
+class UserController {
+  static hashP() {
+    console.log("1", 1);
+    return 1;
+  }
   static async register(req: Request, res: Response) {
     const { username, password } = req.body;
 
@@ -23,6 +34,38 @@ export class UserController {
         (err: any) => console.log(err)
       );
       res.send("Usuário registrado com sucesso");
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  static async login(req: SignInRequest, res: Response) {
+    const { username, password } = req.body;
+
+    if (!username) throw new Error("Username is required");
+    if (!password) throw new Error("Password is required");
+
+    const [user] = await User.find({ username: username });
+    if (!user) throw new Error("User not registered");
+
+    const { hashedPassword } = user;
+
+    const isPasswordValid = bcrypt.compareSync(password, hashedPassword);
+
+    if (!isPasswordValid) throw new Error("Invalid password");
+
+    try {
+      const { id } = user;
+
+      const paramsToAssignToToken = { user_id: id };
+
+      const jwtSignOptions = { expiresIn: "7d" };
+
+      if (!JWT_SECRET) throw new Error("JWT_SECRET_NOT_FOUND");
+
+      const token = jwt.sign(paramsToAssignToToken, JWT_SECRET, jwtSignOptions);
+
+      res.status(200).send({ access_token: token });
     } catch (err: any) {
       res.status(500).json({ error: err.message });
     }
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import UserController from "../controllers/userController";
 const router = Router();
 
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 export default router;
