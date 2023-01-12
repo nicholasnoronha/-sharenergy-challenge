00148cc32801bb3c517ef8498d4fe4e6dc6f84cf
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-12 12:30:41 -0300

    feat: create http cat route

---
+++ backend/src/routes/httpCatRoutes.ts
@@ -0,0 +1,7 @@
+import HttpCatController from "../controllers/HttpCatController";
+import Router from "express";
+const httpCatRouter = Router();
+
+httpCatRouter.get("", HttpCatController.getStatus);
+
+export default httpCatRouter;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./userRoutes";
 import rugRouter from "./rugRoutes";
+import httpCatRouter from "./httpCatRoutes";
 
 const routes = Router();
 
 routes.use("/user", userRouter);
 routes.use("/rug", rugRouter);
+routes.use("/cat", httpCatRouter);
 
 export default routes;
