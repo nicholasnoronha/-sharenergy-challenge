1e400f2ca0a353e2ba266796affa3a3e09995d97
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-14 17:05:37 -0300

    refactor: mongoose client schema

--- backend/src/models/client.ts
+++ backend/src/models/client.ts
@@ -1,11 +1,33 @@
 import mongoose from "mongoose";
 
 const clientSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: Number,
-  adress: String,
-  CPF: Number,
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  address: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  cpf: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
 });
 
 const ClientModel = mongoose.model("client", clientSchema);
