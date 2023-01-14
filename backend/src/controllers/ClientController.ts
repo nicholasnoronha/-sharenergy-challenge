4da3d155acd9452cb5a69887500c0bb37e9af16d
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-14 20:31:55 -0300

    feat: add get/update/delete client methods

--- backend/src/controllers/ClientController.ts
+++ backend/src/controllers/ClientController.ts
@@ -19,6 +19,65 @@ class ClientController {
       return res.status(500).json({ error: error.message });
     }
   }
+
+  static async getClient(req: Request, res: Response) {
+    const clientId = req.params.id;
+
+    try {
+      const client = await ClientModel.findOne({ _id: clientId });
+
+      return res.status(201).send({ client });
+    } catch (err: unknown) {
+      const error = err as Error;
+
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async getClients(req: Request, res: Response) {
+    try {
+      const clients = await ClientModel.find();
+
+      return res.status(201).send({ clients });
+    } catch (err: unknown) {
+      const error = err as Error;
+
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async updateClient(req: Request, res: Response) {
+    const { name, email, phone, address, cpf } = req.body;
+    const clientId = req.params.id;
+    const clientToUpdate: Client = { name, email, phone, address, cpf };
+
+    try {
+      await ClientModel.findOneAndUpdate({ _id: clientId }, clientToUpdate);
+
+      return res.status(201).send("Cliente atualizado com sucesso");
+    } catch (err: unknown) {
+      const error = err as Error;
+
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async deleteClient(req: Request, res: Response) {
+    const clientId = req.params.id;
+
+    try {
+      const response = await ClientModel.deleteOne({ _id: clientId });
+
+      if (response.deletedCount === 0)
+        return res.status(400).send("Este cliente já foi excluído");
+
+      return res.status(201).send("Cliente excluído com sucesso");
+    } catch (err: unknown) {
+      const error = err as Error;
+
+      return res.status(500).json({ error: error.message });
+    }
+  }
 }
 
 export default ClientController;
--- backend/src/routes/clientRoutes.ts
+++ backend/src/routes/clientRoutes.ts
@@ -2,6 +2,10 @@ import ClientController from "../controllers/ClientController";
 import Router from "express";
 const clientRouter = Router();
 
-clientRouter.get("/add", ClientController.addClient);
+clientRouter.get("", ClientController.getClients);
+clientRouter.get("/:id", ClientController.getClient);
+clientRouter.post("/add", ClientController.addClient);
+clientRouter.put("/edit/:id", ClientController.updateClient);
+clientRouter.delete("/delete/:id", ClientController.deleteClient);
 
 export default clientRouter;
