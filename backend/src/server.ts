449135b60f14eca050d10ec68a0d3a9b9e8e06d1
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-16 21:46:27 -0300

    refactor(backend): authorization middleware file name change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
-import AssignDecoded from "./adapters/AssignDecoded";
+import Authorization from "./adapters/Authorization";
 
 const app = express();
 require("dotenv").config();
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(AssignDecoded.run);
+app.use(Authorization.run);
 app.use(routes);
 
 app.listen(port, () => console.log("Server is running on port: " + port));
