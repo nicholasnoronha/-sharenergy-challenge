a14e8445333888568efd33baf9c9b8f4ddb735a8
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-17 18:37:30 -0300

    feat(frontend): create loading and status image components

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,19 @@
+import loadingGif from "../../assets/loading-gif.gif";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img src={loadingGif} style={{ width: "4rem" }} />
+    </div>
+  );
+};
+
+export default Loading;
