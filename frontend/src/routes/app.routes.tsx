c857819cacddaec4053e6831ac79623c5e85cbd5
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-17 18:38:27 -0300

    feat(fronend): add http status route

--- frontend/src/routes/app.routes.tsx
+++ frontend/src/routes/app.routes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
+import HttpStatus from "../pages/HttpStatus";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/http-status" element={<HttpStatus />} />
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
