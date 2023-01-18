defb08972315db1e641174e5ae9d98696a0279a6
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-18 17:54:55 -0300

    feat(frontend): create random dog page and fetch random dog image

---
+++ frontend/src/pages/RandomDog/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useAuth } from "../../contexts/auth.context";
+import Loading from "../../components/Loading";
+import { Button } from "../../components";
+import { randomDogService } from "../../services/randomDogService";
+import SuspenseImageOrGif from "../../components/SuspenseImageOrGif";
+
+const RandomDog: React.FC = () => {
+  const { token } = useAuth();
+  const [randomDogImageOrGif, setRandomDogImageOrGif] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = () => {
+    fetchRandomDog();
+  };
+
+  const fetchRandomDog = async () => {
+    setRandomDogImageOrGif("");
+    setIsLoading(true);
+
+    const response = await randomDogService.getRandomImageOrGif(token!);
+
+    if (!response) return alert("404 Not Found");
+    console.log("response.data", response.data);
+    setRandomDogImageOrGif(response.data);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClick}>Clique Aqui!!!</Button>
+      <React.Suspense fallback={isLoading && <Loading />}>
+        <SuspenseImageOrGif
+          src={randomDogImageOrGif}
+          height="500"
+          width={400}
+        />
+      </React.Suspense>
+    </div>
+  );
+};
+
+export default RandomDog;
--- frontend/src/routes/app.routes.tsx
+++ frontend/src/routes/app.routes.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
+import Loading from "../components/Loading";
+// import Dashboard from "../pages/Dashboard";
+const Dashboard = React.lazy(() => import("../pages/Dashboard"));
 import HttpStatus from "../pages/HttpStatus";
+import RandomDog from "../pages/RandomDog";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <React.Suspense fallback={<Loading />}>
+            <Dashboard />
+          </React.Suspense>
+        }
+      />
       <Route path="/http-status" element={<HttpStatus />} />
+      <Route path="/dog" element={<RandomDog />} />
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
