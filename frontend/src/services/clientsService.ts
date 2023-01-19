5faeb05a942a0615faf56f5b4a76bb385a3a7c61
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-18 22:24:42 -0300

    feat(frontend): create client service

---
+++ frontend/src/services/clientsService.ts
@@ -0,0 +1,83 @@
+import axios, { AxiosError } from "axios";
+import Client from "../interfaces/Client";
+import { API } from "./API";
+
+export class clientsService {
+  static async getClients(access_token: string) {
+    try {
+      const response = await axios.get(`${API}/client`, {
+        headers: {
+          Authorization: access_token,
+        },
+      });
+
+      return response.data.clients;
+    } catch (err: unknown) {
+      const error = err as AxiosError;
+      console.log("error", error);
+      alert(error.message);
+    }
+  }
+
+  static async getClient(access_token: string, user_id: string) {
+    try {
+      return await axios.get(`${API}/client/${user_id}`, {
+        headers: {
+          Authorization: access_token,
+        },
+      });
+    } catch (err: unknown) {
+      const error = err as AxiosError;
+      console.log("error", error);
+      alert(error.message);
+    }
+  }
+
+  static async addClient(access_token: string, client: Client) {
+    try {
+      return await axios.post(`${API}/client/add`, {
+        headers: {
+          Authorization: access_token,
+        },
+        client,
+      });
+    } catch (err: unknown) {
+      const error = err as AxiosError;
+      console.log("error", error);
+      alert(error.message);
+    }
+  }
+
+  static async updateClient(
+    access_token: string,
+    client: Client,
+    user_id: string
+  ) {
+    try {
+      return await axios.post(`${API}/client/edit/${user_id}`, {
+        headers: {
+          Authorization: access_token,
+        },
+        client,
+      });
+    } catch (err: unknown) {
+      const error = err as AxiosError;
+      console.log("error", error);
+      alert(error.message);
+    }
+  }
+
+  static async deleteClient(access_token: string, user_id: string) {
+    try {
+      return await axios.delete(`${API}/client/delete/${user_id}`, {
+        headers: {
+          Authorization: access_token,
+        },
+      });
+    } catch (err: unknown) {
+      const error = err as AxiosError;
+      console.log("error", error);
+      alert(error.message);
+    }
+  }
+}
